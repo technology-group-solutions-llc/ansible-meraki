29c9bf048eebf6c1b0df9934955c8392bc898962
Aaron Norton <anorton@tgs-mtc.com>
2023-05-31 13:52:10 -0500

    python script to output devices yaml from csv

---
+++ scripts/create_yaml_from_csv.py
@@ -0,0 +1,58 @@
+import csv
+import yaml
+
+def create_yaml_file(csv_file, yaml_file):
+  """Creates a YAML file from a CSV file.
+
+  Args:
+    csv_file: The path to the CSV file.
+    yaml_file: The path to the YAML file.
+
+  """
+
+  new_data_yaml = {}
+
+  with open(csv_file, 'r') as csv_file_reader:
+    reader = csv.DictReader(csv_file_reader)
+    data = [row for row in reader if row['Site Name'] == site_name]
+    new_data = [{k:v for k,v in d.items() if k in ['Site Name', 'Device Name', 'SN']} for d in data]
+  
+  append_dict_title(new_data, new_data_yaml, 'device')
+    
+  with open(yaml_file, 'w') as yaml_file_writer:
+    yaml_file_writer.write("---\n")
+    yaml_writer = yaml.dump(new_data_yaml, yaml_file_writer, default_flow_style=False)
+
+def append_dict_title(list_of_dicts, new_dict, title):
+    """
+    Iterates over a list of dicts and appends an outer dict to each one with an increasing number identifier
+
+    Args:
+    list_of_dicts: The list of dicts to iterate over
+    new_dict: The new dict to append to
+    title: The outer dict title to add to the new dict (ie - "subnet-" would add an outer dict of subnet-1, subnet-2, etc)
+
+    Returns:
+    The updated dict.
+    """
+    for dict in list_of_dicts:
+        identifier = title + "-" + str(len(new_dict) + 1)
+        new_dict[identifier] = dict
+
+    return new_dict
+
+if __name__ == '__main__':
+  print("")
+  print("Run this from the directory where the CSV input file is located")
+  print("This will create a devices.yaml file in the same directory")
+  print("")
+  print("The CSV file needs to have these headings:")
+  print("Site Name, SN, Device Name")
+  print("")
+  csv_file = input("Enter name of the CSV file you would like to use as input: ")
+  print("")
+  print("Next you will be prompted for a Site Name and only devices assigned to this Site Name will be in the yaml output")
+  print("")
+  site_name = input("Which Site Name would you like to output YAML for: ")
+  yaml_file = 'devices.yaml'
+  create_yaml_file(csv_file, yaml_file)
